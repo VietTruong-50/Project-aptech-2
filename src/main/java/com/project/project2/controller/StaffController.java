8b1b6c8e6fefe6a3b20fcdeefc00ab3ae40fa7ff
VietTruong-50 <truongquocviet2001@gmail.com>
2022-08-04 10:34:50 +0700

    update controller

--- src/main/java/com/project/project2/controller/StaffController.java
+++ src/main/java/com/project/project2/controller/StaffController.java
@@ -2,22 +2,22 @@ package com.project.project2.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class StaffController {
     public AnchorPane root;
+    public TextField nameStaffTf;
+    public TextField phoneStaffTf;
+    public TextField contractNbTf;
+    public DatePicker birthStaffDp;
+    public Circle pointLight;
 
-    public void showAddForm(ActionEvent actionEvent) {
-    }
-
-    public void showEditForm(ActionEvent actionEvent) {
-    }
-
-    public void delStaff(ActionEvent actionEvent) {
-    }
 
     public void setGoBackBtn(ActionEvent actionEvent) throws IOException {
         AnchorPane dashboard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/project/project2/MainDashboard.fxml")));
@@ -28,5 +28,14 @@ public class StaffController {
     }
 
     public void importFile(ActionEvent actionEvent) {
+    }
+
+    public void addStaff(ActionEvent actionEvent) {
+    }
+
+    public void updateStaff(ActionEvent actionEvent) {
+    }
+
+    public void delStaff(ActionEvent actionEvent) {
     }
 }
